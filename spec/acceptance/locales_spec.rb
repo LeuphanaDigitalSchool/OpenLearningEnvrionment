320aa680aa43554738e04cbc2362b44c071231ab
Tomasz K <Tomasz.Kulik@akra.net>
2016-08-05 13:38:20 +0200

    change examples documentation

--- spec/acceptance/locales_spec.rb
+++ spec/acceptance/locales_spec.rb
@@ -7,14 +7,14 @@ resource 'Api::V1::Locales' do
   parameter :lang, 'Set language code'
 
   get '/api/v1/locales' do
-    example '#non-logged #change locales to polish' do
+    example '#locales (change locales to polish for non-logged)' do
       explanation 'Change locales to polish language'
       do_request(lang: 'pl')
       expect(response_headers['user-language']).to eq('pl'.to_sym)
       expect(response_status).to eq(200)
     end
 
-    example '#non-logged #change locales to english' do
+    example '#locales (change locales to english for non-logged)' do
       explanation 'Change locales to english language'
       do_request(lang: 'en')
       expect(response_headers['user-language']).to eq('en'.to_sym)
@@ -25,14 +25,14 @@ resource 'Api::V1::Locales' do
   get '/api/v1/locales' do
     let(:user) { FactoryGirl.create(:user, :public_user) }
 
-    example '#logged #change locales to polish' do
+    example '#locales (change locales to polish for logged)' do
       login(user)
       do_request(lang: 'pl')
       expect(response_headers['user-language']).to eq('pl'.to_sym)
       expect(response_status).to eq(200)
     end
 
-    example '#logged #change locales to english' do
+    example '#locales (change locales to english for logged)' do
       login(user)
       do_request(lang: 'en')
       expect(response_headers['user-language']).to eq('en'.to_sym)
