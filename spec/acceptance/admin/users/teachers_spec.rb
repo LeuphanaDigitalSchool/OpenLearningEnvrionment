508dcb82be7b9b3bbd528bcfd5dd8f8b2969c861
Tomasz K <Tomasz.Kulik@akra.net>
2016-08-09 10:52:07 +0200

    edit teacher profile

--- spec/acceptance/admin/users/teachers_spec.rb
+++ spec/acceptance/admin/users/teachers_spec.rb
@@ -82,6 +82,64 @@ resource 'Api::V1::Admin::Users::Teachers' do
     end
   end
 
+  put '/api/v1/admin/users/teachers/:id' do
+    parameter :id, 'teacher id', required: true
+    parameter :title, 'Title', required: false
+    parameter :firstname, 'Firstname', required: true
+    parameter :lastname, 'Lastname', required: true
+    parameter :gender, 'Gender', required: false
+    parameter :country, 'country', required: false
+    parameter :birthdate, 'Date of birth', required: false
+    parameter :educational_attainment, 'Educational attainment', required: false
+    parameter :profession, 'Profession', required: false
+    parameter :avatar, 'Avatar', required: false
+    parameter :interests, 'Interests', required: false
+    parameter :introduction, 'Introduction', required: false
+    parameter :email, 'E-mail', required: false
+    parameter :password, 'Password', required: true
+    parameter :password_confirmation, 'Password confirmation', required: true
+
+    let(:raw_post) { params.to_json }
+    let(:teacher_params) do
+      { 'id': teacher.id, 'teacher': { 'firstname': 'Nash', 'lastname': 'Forbes', 'title': 'Math' } }
+    end
+
+    example '#update (request not authorized)', document: false do
+      do_request(teacher_params)
+      expect(response_body).to include('errors')
+      expect(response_status).to be 401
+    end
+
+    example '#update (request authorized)' do
+      login(user)
+      do_request(teacher_params)
+      expect(response_body).to include('Nash', 'Forbes')
+      expect(response_status).to be 200
+    end
+  end
+
+  delete '/api/v1/admin/users/teachers/:id' do
+    parameter :id, 'Teacher id', required: true
+    parameter :deleted, 'Delete/deactivate teacher', required: false
+
+    let(:raw_post) { params.to_json }
+    let(:teacher_params) do
+      { 'id': teacher.id, 'teacher': { 'deleted': 'true' } }
+    end
+
+    example '#delete (request not authorized)', document: false do
+      do_request(teacher_params)
+      expect(response_body).to include('errors')
+      expect(response_status).to be 401
+    end
+
+    example '#delete (request authorized)' do
+      login(user)
+      do_request(teacher_params)
+      expect(response_status).to be 204
+    end
+  end
+
   private
 
   def teacher_empty_params
