95cae425d3697a3ce63678e812671912f6f8cc5b
slomagnieszka <agnieszka.sloma@akra.net>
2016-08-11 09:00:30 +0200

    Create courses migration

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,50 +10,69 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_160_808_135_921) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+ActiveRecord::Schema.define(version: 20160811064438) do
 
-  create_table 'users', force: :cascade do |t|
-    t.string   'provider',               default: 'email', null: false
-    t.string   'uid',                    default: '',      null: false
-    t.string   'encrypted_password',     default: '',      null: false
-    t.string   'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.integer  'sign_in_count', default: 0, null: false
-    t.datetime 'current_sign_in_at'
-    t.datetime 'last_sign_in_at'
-    t.string   'current_sign_in_ip'
-    t.string   'last_sign_in_ip'
-    t.string   'confirmation_token'
-    t.datetime 'confirmed_at'
-    t.datetime 'confirmation_sent_at'
-    t.string   'unconfirmed_email'
-    t.string   'email'
-    t.string   'language', default: 'en'
-    t.json     'tokens'
-    t.datetime 'created_at',                               null: false
-    t.datetime 'updated_at',                               null: false
-    t.string   'title'
-    t.string   'firstname'
-    t.string   'lastname'
-    t.integer  'gender'
-    t.string   'country'
-    t.integer  'educational_attainment'
-    t.string   'profession'
-    t.string   'avatar'
-    t.text     'interests'
-    t.string   'timezone'
-    t.text     'introduction'
-    t.boolean  'data_privacy',           default: false
-    t.boolean  'terms_and_conditions',   default: false
-    t.boolean  'honor_code',             default: false
-    t.integer  'role',                   default: 0
-    t.date     'birthdate'
-    t.boolean  'deleted', default: false
-    t.index ['email'], name: 'index_users_on_email', using: :btree
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true, using: :btree
-    t.index %w(uid provider), name: 'index_users_on_uid_and_provider', unique: true, using: :btree
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "courses", force: :cascade do |t|
+    t.string   "title",       default: "", null: false
+    t.text     "description"
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "provider",               default: "email", null: false
+    t.string   "uid",                    default: "",      null: false
+    t.string   "encrypted_password",     default: ""
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,       null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.string   "email"
+    t.string   "language",               default: "en"
+    t.json     "tokens"
+    t.datetime "created_at",                               null: false
+    t.datetime "updated_at",                               null: false
+    t.string   "title"
+    t.string   "firstname"
+    t.string   "lastname"
+    t.integer  "gender"
+    t.string   "country"
+    t.integer  "educational_attainment"
+    t.string   "profession"
+    t.string   "avatar"
+    t.text     "interests"
+    t.string   "timezone"
+    t.text     "introduction"
+    t.boolean  "data_privacy",           default: false
+    t.boolean  "terms_and_conditions",   default: false
+    t.boolean  "honor_code",             default: false
+    t.integer  "role",                   default: 0
+    t.date     "birthdate"
+    t.boolean  "deleted",                default: false
+    t.string   "invitation_token"
+    t.datetime "invitation_created_at"
+    t.datetime "invitation_sent_at"
+    t.datetime "invitation_accepted_at"
+    t.integer  "invitation_limit"
+    t.integer  "invited_by_id"
+    t.string   "invited_by_type"
+    t.index ["email"], name: "index_users_on_email", using: :btree
+    t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true, using: :btree
+  end
+
 end
