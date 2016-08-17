53df6bdc99e3c96c698c9e8284be0a2eea009626
Piotr Koson <pior.koson@akra.net>
2016-08-17 11:17:09 +0200

    Add simple passwords verification

--- client/app/routes/admin/createProfile/CreateProfileCtrl.js
+++ client/app/routes/admin/createProfile/CreateProfileCtrl.js
@@ -1,12 +1,11 @@
 export default class CreateProfileCtrl {
-  constructor(countryList, $state, toastr, Restangular) {
+  constructor(countryList, $state, toastr, Restangular, Upload) {
     "ngInject";
-    let userType;
-    this.user = null;
+    this.user = {};
     this.toastr = toastr;
     this.$state = $state;
+    this.Upload = Upload;
     this.Restangular = Restangular;
-    this.baseLephanaUser = this.Restangular.all('/api/v1/admin/users');
     this.genders = [
       'male',
       'female',
@@ -33,15 +32,17 @@ export default class CreateProfileCtrl {
   submit(form, data){
     this.errors = 0;
     this.handleError(form, data);
+    this.checkUserType();
+    this.baseLephanaUser = this.Restangular.all('/api/v1/admin/users/' + this.userType);
     if(this.errors === 0) {
       this.send = false;
-      this.baseLephanaUser.post(userType, this.user).then(()=>{
-        this.send = true;
-        this.toastr.success('Thank you for registering', 'Success');
-        this.loginPage();
+      this.baseLephanaUser.post(this.sendData).then(()=>{
+        this.toastr.success('Thank you for registering new user', 'Success');
+        this.initialData();
       }, (response)=> {
         this.send = true;
-        let status = response.data.errors.full_messages;
+        console.log(response);
+        let status = response.statusText;
         for (var i = 0; i < status.length; i++) {
           this.toastr.error(status[i], 'Error');
         }
@@ -49,15 +50,49 @@ export default class CreateProfileCtrl {
     }
   }
   checkUserType(){
-    if(this.role === 'student'){userType = 'students';}
-    if(this.role === 'teacher'){userType = 'teachers';}
-    if(this.role === 'course director'){userType = 'course_directors';}
-    if(this.role === 'support'){userType = 'supports';}
+    if(this.role === 'student'){
+      this.userType = 'students';
+      this.sendData = {'student':{ }};
+      this.sendData.student = this.user;
+    }
+    if(this.role === 'teacher'){
+      this.userType = 'teachers';
+      this.sendData = {'teacher':{ }};
+      this.sendData.teacher = this.user;
+    }
+    if(this.role === 'course director'){
+      this.userType = 'course_directors';
+      this.sendData = {'course_director':{ }};
+      this.sendData.course_director = this.user;
+    }
+    if(this.role === 'support'){
+      this.userType = 'supports';
+      this.sendData = {'support':{ }};
+      this.sendData.support = this.user;
+    }
   }
 
   handleError(form, data){
-    if(form.$error.required){
-      this.toastr.error('You need to fill all required fields', 'Error');
+    if(this.role === 'student'){
+      this.handleErrorStudents();
+    }
+    if(this.user.password !== this.user.password_confirmation) {
+      this.toastr.error('Password Must Match', 'Error');
+      this.errors += 1;
+    }
+  }
+
+  handleErrorStudents() {
+    if(!this.user.gender) {
+      this.toastr.error('You need to select gender', 'Error');
+      this.errors += 1;
+    }
+    if(!this.user.country) {
+      this.toastr.error('You need to select country', 'Error');
+      this.errors += 1;
+    }
+    if(!this.user.birthdate) {
+      this.toastr.error('You need to fill birth date', 'Error');
       this.errors += 1;
     }
     if(!this.user.honor_code) {
@@ -72,5 +107,42 @@ export default class CreateProfileCtrl {
       this.toastr.error('You need to accept data privacy', 'Error');
       this.errors += 1;
     }
+  }
+
+  uploadImage(file, errFiles, $event) {
+      if (file) {
+          this.uploading = true;
+          file.upload = this.Upload.base64DataUrl(file);
+          file.upload.then( (response) => {
+            this.user.avatar = response;
+            this.uploading = false;
+          }, (response)=> {
+              if (response.status > 0)
+                  $scope.errorMsg = response.status + ': ' + response.data;
+          }, (evt)=> {
+            file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+          });
+      }
+      if (errFiles[0]) {
+        this.$mdDialog.show(
+          this.$mdDialog.alert()
+            .clickOutsideToClose(true)
+            .title('Image size')
+            .textContent('Yours file size is wrong (max height & width = 1000 px)')
+            .ariaLabel('file size')
+            .ok('ok')
+            .targetEvent($event)
+        );
+      }
+  }
+
+  removeImage() {
+    this.user.avatar = null;
+  }
+  initialData() {
+    this.send = true;
+    this.sendData = {};
+    this.user = {};
+    this.role = null;
   }
 }
