72a6b0f65ee8111740e82611587009a8777a167f
slomagnieszka <agnieszka.sloma@akra.net>
2016-08-18 08:23:09 +0200

    Create uploader for storage

--- app/models/storage.rb
+++ app/models/storage.rb
@@ -4,5 +4,8 @@ class Storage < ApplicationRecord
   belongs_to :user
   belongs_to :course
 
+  mount_base64_uploader :file, StorageUploader
+
   validates :source, presence: true
+
 end
---
+++ app/uploaders/storage_uploader.rb
@@ -0,0 +1,22 @@
+# encoding: utf-8
+# frozen_string_literal: true
+# StorageUploader
+class StorageUploader < CarrierWave::Uploader::Base
+  storage :file
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{model.id}/#{mounted_as}"
+  end
+
+  def extension_white_list
+    %w(pdf jpg mp3 mp4)
+  end
+
+  def filename
+    if original_filename
+      timestamp = Time.now.strftime('%Y%m%d%H%M%S')
+      extension = File.extname(file.file)
+      "#{model.id}_#{timestamp}#{extension}"
+    end
+  end
+end
