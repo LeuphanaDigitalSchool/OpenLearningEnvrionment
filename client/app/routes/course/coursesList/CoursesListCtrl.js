2199cbf8eb4b6c5f3f99aa3e9209f8963c46b297
Piotr Koson <pior.koson@akra.net>
2016-08-30 14:18:04 +0200

    Add course list

---
+++ client/app/routes/course/coursesList/CoursesListCtrl.js
@@ -0,0 +1,19 @@
+export default class CoursesListCtrl {
+  constructor(Restangular) {
+    "ngInject";
+    this.Restangular = Restangular;
+    this.coursesApi = this.Restangular.oneUrl('courses', '/api/v1/courses');
+    this.coursesList();
+}
+
+  coursesList() {
+    this.coursesApi.get().then(
+      (response)=>{
+        this.coursesList = response.courses;
+    });
+  }
+
+  deleteCourse(courseId){
+    this.Restangular.oneUrl('courses', '/api/v1/courses/'+courseId).remove();
+  }
+}
