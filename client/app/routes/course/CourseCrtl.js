e2fcce305ba981cbce796bf74f55478df3cb0694
Piotr Koson <pior.koson@akra.net>
2016-08-31 09:54:52 +0200

    Add possibility to edit course

--- client/app/routes/course/CourseCrtl.js
+++ client/app/routes/course/CourseCrtl.js
@@ -1,17 +1,43 @@
 export default class CourseCrtl {
-  constructor(Restangular, $stateParams) {
+  constructor(Restangular, $stateParams, $filter, toastr, $scope) {
     "ngInject";
     this.Restangular = Restangular;
     this.$stateParams = $stateParams;
     this.courseId = $stateParams.id;
+    this.$scope = $scope;
+    this.toastr = toastr;
     this.coursesApi = this.Restangular.oneUrl('course', '/api/v1/courses/'+this.courseId);
     this.course();
+    this.edit = false;
+    this.data= {};
   }
   course() {
     this.coursesApi.get().then(
     (response)=>{
-      this.course = response.course;
-      console.log('this.course', this.course);
+      console.log('response', response);
+      this.$scope.courseData = response.course;
+    });
+  }
+  editFn() {
+    if(this.edit){
+      this.submit();
+    } else {
+      this.edit = true;
+    }
+  }
+
+  submit(){
+    this.baseLephanaUser = this.Restangular.all('/api/v1/courses/');
+    this.data.course=this.$scope.courseData;
+    this.sending = true;
+    this.baseLephanaUser.customPUT(this.data, this.courseId).then(()=>{
+      this.toastr.success('Thank you for edditing course', 'Success');
+      this.edit = false;
+    }, (response)=> {
+      this.sending = false;
+      this.edit = false;
+      let status = response.statusText;
+      this.toastr.error(status, 'Error');
     });
   }
 }
