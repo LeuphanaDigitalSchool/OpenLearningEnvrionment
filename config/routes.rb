ce6ef269fa813c3f8619637734d225c492b14b1b
Tomasz Kulik <kulisz@kulisz.eu>
2016-09-12 19:30:58 +0200

    Update storages routes

--- config/routes.rb
+++ config/routes.rb
@@ -28,7 +28,7 @@ Rails.application.routes.draw do
       end
 
       resources :courses do
-        resources :storages, only: [:create]
+        resources :storages, only: [:create, :delete]
         resources :course_phases, only: [:index, :show, :update], path: 'phases' do
           resources :storages
           get 'preferences/:role' => 'course_phases#preferences'
