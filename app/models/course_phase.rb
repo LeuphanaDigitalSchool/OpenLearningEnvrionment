ad65f85f94099ff70178b532869f4517f2bd85b1
Tomasz K <Tomasz.Kulik@akra.net>
2016-09-14 15:21:01 +0200

    add validations

--- app/models/course_phase.rb
+++ app/models/course_phase.rb
@@ -3,11 +3,12 @@
 class CoursePhase < ApplicationRecord
   belongs_to :course
   has_and_belongs_to_many :storages
-  has_many :course_phase_preferences
+  has_many :course_phase_preferences, dependent: :destroy
   accepts_nested_attributes_for :course_phase_preferences
 
   default_scope { order(id: :asc, start_date: :asc) }
   scope :active, -> { where('? > start_date AND end_date > ?', Time.now, Time.now).first }
 
   validates :title, presence: true
+  validates :start_date, :end_date, presence: true
 end
