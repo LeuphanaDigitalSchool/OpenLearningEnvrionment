b4f8c81bc8cfc34c9882270c36fc0174510f1878
Tomasz K <Tomasz.Kulik@akra.net>
2016-09-15 10:30:00 +0200

    fix show storage for active phase

---
+++ db/migrate/20160915074753_change_column_type_in_course_phases.rb
@@ -0,0 +1,11 @@
+class ChangeColumnTypeInCoursePhases < ActiveRecord::Migration[5.0]
+  def up
+    change_column :course_phases, :start_date, :date
+    change_column :course_phases, :end_date, :date
+  end
+
+  def down
+    change_column :course_phases, :start_date, :datetime
+    change_column :course_phases, :end_date, :datetime
+  end
+end
