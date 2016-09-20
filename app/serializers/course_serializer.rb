b28a5a4861f2399f4f01be1fc5213900d793fb32
slomagnieszka <agnieszka.sloma@akra.net>
2016-09-20 14:13:14 +0200

    Change course title in side nav

--- app/serializers/course_serializer.rb
+++ app/serializers/course_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 # CourseSerializer
 class CourseSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :start_date, :end_date
+  attributes :id, :title, :description, :start_date, :end_date, :short_title
   has_many :course_phases, serializer: CoursePhaseSerializer
 
   def start_date
@@ -10,5 +10,13 @@ class CourseSerializer < ActiveModel::Serializer
 
   def end_date
     object.end_date.strftime('%Y-%m-%d') if object.end_date
+  end
+
+  def short_title
+    if object.title.size > 21
+      object.title[0,20] + "..." 
+    else
+      object.title
+    end
   end
 end
