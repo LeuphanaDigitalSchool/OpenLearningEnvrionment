d22df45f574bf183f509daad366edf8a7784dfd9
Piotr Koson <pior.koson@akra.net>
2016-09-26 14:27:26 +0200

    Add choose all swtich button to video component

--- client/app/routes/course/createCourse/communication/videocomponent/video.component.js
+++ client/app/routes/course/createCourse/communication/videocomponent/video.component.js
@@ -1,6 +1,17 @@
 class VideoCtrl {
   constructor() {
     "ngInject";
+    this.status = false;
+  }
+  chooseAllVideo(){
+    this.status = !this.status;
+    this.scheduleVideoMeetings = this.status;
+    this.sendOutInvitesToVideo = this.status;
+    this.defineVisibilityOfVideoMeeting = this.status;
+    this.public = this.status;
+    this.privateAmongParticipants = this.status;
+    this.recordMeetings = this.status;
+    this.shareRecording = this.status;
   }
 }
 
