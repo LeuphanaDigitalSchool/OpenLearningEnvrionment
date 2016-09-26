ef46b9440a1f08c2967aaaf036f686aa96df1bf6
Piotr Koson <pior.koson@akra.net>
2016-09-26 15:09:19 +0200

    Add choose all switch button to forum component

--- client/app/routes/course/createCourse/communication/forum/forum.component.js
+++ client/app/routes/course/createCourse/communication/forum/forum.component.js
@@ -1,6 +1,30 @@
 class ForumCtrl {
   constructor() {
     "ngInject";
+    this.status = false;
+  }
+  chooseAllForum(){
+    this.status = !this.status;
+    this.threadedConversations = this.status;
+    this.taggingOfUsers = this.status;
+    this.forumNotifications = this.status;
+    this.emailNotifications = this.status;
+    this.taggingOfUserGroups = this.status;
+    this.forumTeachers = this.status;
+    this.forumStudents = this.status;
+    this.TaggingForumNotifications = this.status;
+    this.emailNotifications = this.status;
+    this.quoteFromForums = this.status;
+    this.embedContentInForumPost = this.status;
+    this.video = this.status;
+    this.pictures = this.status;
+    this.upvotingDownvoting = this.status;
+    this.createTopics = this.status;
+    this.createThreads = this.status;
+    this.sortForum = this.status;
+    this.filterForum = this.status;
+    this.embedExternalLinks = this.status;
+    this.embedInternalLinks = this.status;
   }
 }
 
