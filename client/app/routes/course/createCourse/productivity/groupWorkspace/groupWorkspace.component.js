9109191abe8e2006fc239aca70e1500d5f905d11
Piotr Koson <pior.koson@akra.net>
2016-09-27 09:44:54 +0200

    Add choose all switch button to workspace

--- client/app/routes/course/createCourse/productivity/groupWorkspace/groupWorkspace.component.js
+++ client/app/routes/course/createCourse/productivity/groupWorkspace/groupWorkspace.component.js
@@ -1,6 +1,27 @@
 class GroupWorkspaceCtrl {
   constructor() {
     "ngInject";
+    this.status = false;
+  }
+  chooseAllWorkspace(){
+    this.status = !this.status;
+    this.groupDefineVisibility = this.status;
+    this.groupShareWorkspaceWithOthers = this.status;
+    this.groupFileSharing = this.status;
+    this.groupEmbedFromExternalWebsites = this.status;
+    this.groupShareOwnMaterialOnPlatform = this.status;
+    this.groupPublishingOfDrafts = this.status;
+    this.groupPublishingOfFinalSubmission = this.status;
+    this.groupDefineVisibilityOfFiles = this.status;
+    this.groupCalendar = this.status;
+    this.groupGroupCalendar = this.status;
+    this.groupShowCoursePhases = this.status;
+    this.groupCoursePhases = this.status;
+    this.groupTeacherDefineCourseStartEnd = this.status;
+    this.groupDefineCoursePhases = this.status;
+    this.groupDefineFrozenContent = this.status;
+    this.groupMatchPlatformContentWithEespectivePhase = this.status;
+    this.groupDefineNewSectionsInEach = this.status;
   }
 }
 
--- client/app/routes/course/createCourse/productivity/personalWorkspace/personalWorkspace.component.js
+++ client/app/routes/course/createCourse/productivity/personalWorkspace/personalWorkspace.component.js
@@ -1,6 +1,29 @@
 class PersonalWorkspaceCtrl {
   constructor() {
     "ngInject";
+    this.status = false;
+  }
+  chooseAllWorkspace(){
+    this.status = !this.status;
+    this.personalDefineVisibility = this.status;
+    this.personalShareWorkspaceWithOthers = this.status;
+    this.personalFileSharing = this.status;
+    this.personalEmbedFromExternalWebsites = this.status;
+    this.personalShareOwnMaterialOnPlatform = this.status;
+    this.personalPublishingOfDrafts = this.status;
+    this.personalPublishingOfFinalSubmission = this.status;
+    this.personalDefineVisibilityOfFiles = this.status;
+    this.personalCalendar = this.status;
+    this.personalPersonalCalendar = this.status;
+    this.personalGroupCalendar = this.status;
+    this.personalShowCoursePhases = this.status;
+    this.personalCoursePhases = this.status;
+    this.personalShowCoursePhases = this.status;
+    this.personalTeacherDefineCourseStartEnd = this.status;
+    this.personalDefineCoursePhases = this.status;
+    this.personalDefineFrozenContent = this.status;
+    this.personalMatchPlatformContentWithEespectivePhase = this.status;
+    this.personalDefineNewSectionsInEach = this.status;
   }
 }
 
