273ce4e78c3a898fba80956447235cda6009d5d2
Piotr Koson <pior.koson@akra.net>
2016-09-27 07:53:05 +0200

    Add choose all switch button to email component

--- client/app/routes/course/createCourse/communication/email/email.component.js
+++ client/app/routes/course/createCourse/communication/email/email.component.js
@@ -1,6 +1,17 @@
 class EmailCtrl {
   constructor() {
     "ngInject";
+    this.status = false;
+  }
+  chooseAllEmail(){
+    this.status = !this.status;
+    this.notificationsByEmail = this.status;
+    this.sendUserMessagesAsEmailOptional = this.status;
+    this.sendMessagesViaEmailWithinPlatform = this.status;
+    this.emailNotifications = this.status;
+    this.sendMessagesToUserGroups = this.status;
+    this.replyToForumPostsViaEmail = this.status;
+    this.inviteUserToPlatformByEmail = this.status;
   }
 }
 
